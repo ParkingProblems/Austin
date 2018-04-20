3df6b0707a2c9ea68377a419d91a84f5a1baf28d
jlorrey <jlorrey@mit.edu>
2018-04-20 10:43:23 -0400

    Inital commit of ParseParkingData.js & run.html

---
+++ ParseParkingData.js
@@ -0,0 +1,7 @@
+// Jocelyn Lorrey & Deanna Delgado
+// April 20, 2018
+// ParseParkingData.js 
+// Inputs: austin201x.json file
+// Purpose: pick out the parking violations & keep track of how many & what time
+// Outputs: Array of {meter violations: #, illegal parking spots: #, ..., etc.}
+
